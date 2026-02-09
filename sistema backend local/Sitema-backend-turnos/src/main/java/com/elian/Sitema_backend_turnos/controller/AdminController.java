736241eb685fb67f93fb333a3e3cbd972310a390
EliaNFe <elianferreyra01@gmail.com>
2026-02-09 03:31:48 -0300

    dash de admin con boton de crear turno funcionando

--- sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/controller/AdminController.java
+++ sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/controller/AdminController.java
@@ -1,11 +1,15 @@
 package com.elian.Sitema_backend_turnos.controller;
 
+import com.elian.Sitema_backend_turnos.dto.CrearTurnoDTO;
 import com.elian.Sitema_backend_turnos.service.TurnoService;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
 
 @Controller
 @RequestMapping("/admin")
@@ -28,6 +32,23 @@ public class AdminController {
 
         return "admin-dashboard";
     }
+
+    @GetMapping("/turnos/nuevo")
+    public String mostrarFormularioTurno(Model model) {
+        model.addAttribute("turno", new CrearTurnoDTO(null, null, null, null));
+        return "turno-form";
+    }
+
+
+    @PostMapping("/turnos/nuevo")
+    public String crearTurno(@ModelAttribute CrearTurnoDTO dto) {
+
+        turnoService.crearTurno(dto);
+
+        return "redirect:/admin/dashboard";
+    }
+
+
 
     @GetMapping("/turnos")
     public String verTurnos(Model model) {
--- sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/dto/CrearTurnoDTO.java
+++ sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/dto/CrearTurnoDTO.java
@@ -2,6 +2,7 @@ package com.elian.Sitema_backend_turnos.dto;
 
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDate;
 import java.time.LocalTime;
@@ -16,8 +17,10 @@ public record CrearTurnoDTO(
 
         @NotNull(message = "fecha obligatoria")
         @FutureOrPresent(message = "la fecha no puede ser pasada")
+        @DateTimeFormat(pattern = "yyyy-MM-dd")
         LocalDate fecha,
 
         @NotNull(message = "hora obligatoria")
+        @DateTimeFormat(pattern = "HH:mm")
         LocalTime hora
 ) {}
