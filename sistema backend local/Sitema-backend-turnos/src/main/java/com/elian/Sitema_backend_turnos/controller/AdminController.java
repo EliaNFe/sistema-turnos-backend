7bdc1f083c36e9d7747a26137a14be5c3700122d
EliaNFe <elianferreyra01@gmail.com>
2026-02-12 18:57:21 -0300

    arreglos css

--- sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/controller/AdminController.java
+++ sistema backend local/Sitema-backend-turnos/src/main/java/com/elian/Sitema_backend_turnos/controller/AdminController.java
@@ -92,7 +92,7 @@ public class AdminController {
 
     @GetMapping("/clientes")
     public String listarClientes(@RequestParam(defaultValue = "0") int page,
-                                 @RequestParam(defaultValue = "5") int size,
+                                 @RequestParam(defaultValue = "10") int size,
                                  Model model) {
 
         Page<ClienteDTO> clientesPage = clienteService.listarClientes(PageRequest.of(page, size));
